e0c4e65b88141bb1fdc5508ee1cbb99050b7e43a
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2022-05-14 23:04:57 -0700

    more persisting

--- src/App.java
+++ src/App.java
@@ -15,7 +15,6 @@ public class App {
         EntityManager em = factory.createEntityManager();
 
         /************Instantiating Data***********/
-        //Add Features to database
         em.getTransaction().begin();
         //Add Features to database
             Feature leatherSeats = new Feature("Leather Seats");
@@ -27,23 +26,19 @@ public class App {
             em.persist(leatherSeats);
             Feature awd = new Feature("All-Wheel Drive");
             Feature adaptiveCruise = new Feature("Adaptive Cruise Control");
-
         //Add Packages to database
             Package theater = new Package("Theater Package");
             Package amazon = new Package("Amazon Theater Package");
             Package safety = new Package ("Safety Package");
-
         //Add PackageFeatures to database
             PackageFeature pf1 = new PackageFeature(theater, rearScreens);
             PackageFeature pf2 = new PackageFeature(amazon, rearScreens);
             PackageFeature pf3 = new PackageFeature(amazon, fireTV);
             PackageFeature pf4 = new PackageFeature(safety, adaptiveCruise);
-
         //Add Models to database
             Model pacifica = new Model("Pacifica", 2022);
             Model pacificaHybrid21 = new Model("Pacifica Hybrid", 2022);
             Model pacificaHybrid22 = new Model("Pacifica Hybrid", 2021);
-
         //Add ModelFeatures to database
             //22Pacifica
                 ModelFeature mf1 = new ModelFeature(pacifica, powerDoors);
@@ -53,7 +48,6 @@ public class App {
                 ModelFeature mf4 = new ModelFeature(pacificaHybrid22, hybridEngine);
             //22Hybrid
                 ModelFeature mf5 = new ModelFeature(pacificaHybrid22, powerDoors);
-
         //Add Trims to database
             //22 Gas
                 Trim t1 = new Trim("Touring", 30000, pacifica);
@@ -67,14 +61,6 @@ public class App {
                 Trim t7 = new Trim("Touring", 41000, pacificaHybrid21);
                 Trim t8 = new Trim("Limited", 46000, pacificaHybrid21);
                 Trim t9 = new Trim("Pinnacle", 52000, pacificaHybrid21);
-
-        //Add AvailablePackages to database
-            AvailablePackage ap1 = new AvailablePackage(3000, t1, safety);
-            AvailablePackage ap2 = new AvailablePackage(2500, t2, amazon);
-            AvailablePackage ap3 = new AvailablePackage(2500, t5, amazon);
-            AvailablePackage ap4 = new AvailablePackage(3000, t7, safety);
-            AvailablePackage ap5 = new AvailablePackage(2500, t8, theater);
-            AvailablePackage ap6 = new AvailablePackage(2000, t8, safety);
         //Add TrimFeatures to database
             //22 Limited
                 TrimFeature tf1 = new TrimFeature(t2, leatherSeats);
@@ -101,10 +87,16 @@ public class App {
                 TrimFeature tf17 = new TrimFeature(t9, handsFreeDoors);
                 TrimFeature tf18 = new TrimFeature(t9, rearScreens);
                 TrimFeature tf19 = new TrimFeature(t9, adaptiveCruise);
+        //Add AvailablePackages to database
+            AvailablePackage ap1 = new AvailablePackage(3000, t1, safety);
+            AvailablePackage ap2 = new AvailablePackage(2500, t2, amazon);
+            AvailablePackage ap3 = new AvailablePackage(2500, t5, amazon);
+            AvailablePackage ap4 = new AvailablePackage(3000, t7, safety);
+            AvailablePackage ap5 = new AvailablePackage(2500, t8, theater);
+            AvailablePackage ap6 = new AvailablePackage(2000, t8, safety);
         //Add Automobiles to database
             Automobile a1 = new Automobile("12345abcde");
             a1.setTrim(t2);
-            a1.setPackage();
             Automobile a2 = new Automobile("67890abcde");
             a2.setTrim(t6);
             Automobile a3 = new Automobile("99999aaaaa");
@@ -113,7 +105,7 @@ public class App {
             a4.setTrim(t7);
             Automobile a5 = new Automobile("bbbbb77777");
             a5.setTrim(t6);
-
+        //Persist Features
         em.persist(hybridEngine);
         em.persist(powerDoors);
         em.persist(handsFreeDoors);
@@ -121,19 +113,60 @@ public class App {
         em.persist(rearScreens);
         em.persist(awd);
         em.persist(adaptiveCruise);
+        //Persist Package
         em.persist(theater);
-       // em.persist(pf1);
-        //em.persist(pf2);
+        em.persist(amazon);
+        em.persist(safety);
+        //PersistPackageFeatures
+        em.persist(pf1);
+        em.persist(pf2);
         em.persist(pf3);
-       // em.persist(pf4);
-
-
-
-
-
-
-
-
+        em.persist(pf4);
+        //Persist Models
+        em.persist(pacifica);
+        em.persist(pacificaHybrid21);
+        em.persist(pacificaHybrid22);
+        //Persist ModelFeatures
+        em.persist(mf1);
+        em.persist(mf2);
+        em.persist(mf3);
+        em.persist(mf4);
+        em.persist(mf5);
+        //Persist Trims
+        em.persist(t1);
+        em.persist(t2);
+        em.persist(t3);
+        em.persist(t4);
+        em.persist(t5);
+        em.persist(t6);
+        em.persist(t7);
+        em.persist(t8);
+        em.persist(t9);
+        //Persist TrimFeatures
+        em.persist(tf1);
+        em.persist(tf2);
+        em.persist(tf3);
+        em.persist(tf4);
+        em.persist(tf5);
+        em.persist(tf6);
+        em.persist(tf7);
+        em.persist(tf8);
+        em.persist(tf9);
+        em.persist(tf10);
+        em.persist(tf11);
+        em.persist(tf12);
+        em.persist(tf13);
+        em.persist(tf14);
+        em.persist(tf15);
+        em.persist(tf16);
+        em.persist(tf17);
+        em.persist(tf18);
+        em.persist(tf19);
+        em.persist(a1);
+        em.persist(a2);
+        em.persist(a3);
+        em.persist(a4);
+        em.persist(a5);
         em.getTransaction().commit();
         //Finish adding features to database
     }   
