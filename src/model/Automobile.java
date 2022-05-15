31541b88df9ee0d08224b298c0a3d35a73f3ef72
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2022-05-15 02:47:07 -0700

    Sticker Price is functional; just need getFeatures

--- src/model/Automobile.java
+++ src/model/Automobile.java
@@ -1,4 +1,6 @@
 package model;
+
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -15,49 +17,54 @@ public class Automobile {
     @Column(length = 17, unique = true, nullable = false)
     private String vin;
 
-    //Bidirectional many-to-many between AvailablePackage and Automobiles.
+    // Bidirectional many-to-many between AvailablePackage and Automobiles.
     @ManyToMany
-    @JoinTable(
-        name = "chosenPackage",
-        joinColumns = @JoinColumn(name= "automobile_id"),
-        inverseJoinColumns = @JoinColumn(name="available_id")
-    )
+    @JoinTable(name = "chosenPackage", joinColumns = @JoinColumn(name = "automobile_id"), inverseJoinColumns = @JoinColumn(name = "available_id"))
     private Set<AvailablePackage> availablePackages;
 
-    //Unidirectional many-to-one between Automobiles and Trim
-    //Many automobiles have one trim. Automobiles know trim. Trim don't know automobiles.
+    // Unidirectional many-to-one between Automobiles and Trim
+    // Many automobiles have one trim. Automobiles know trim. Trim don't know
+    // automobiles.
     @ManyToOne
-    @JoinColumn(name="trim_id")
+    @JoinColumn(name = "trim_id")
     private Trim trim;
 
-    //Constructors
+    // Constructors
     public Automobile() {
     }
 
     public Automobile(String vin) {
         this.vin = vin;
+        this.availablePackages = new HashSet<AvailablePackage>();
     }
 
-    //"Adding Application Logic"
-    public Set<Feature> getFeatures(){
-        //
-        Set featureSet = new HashSet<Feature>();
-        this.getTrim().getTrimFeatures();
-        this.getTrim().getModel().getModelFeatures();
-        this.getAvailablePackages().
+    // "Adding Application Logic"
 
+    public Set<Feature> getFeatures() {
+
+        Set<Feature> featureSet = new
+        // Get features from trim
+        featureSet.addAll(this.getTrim().getTrimFeatures());
+        // Get features from model
+        featureSet.addAll(this.getTrim().getModel().getModelFeatures());
+        // Get features from all installed packages
+        for (AvailablePackage P : this.getAvailablePackages()) {
+            featureSet.addAll(P.getPackage().getPackageFeatures());
+        }
     }
-    public double stickerPrice(){
-        //Needs to be tested.
+
+    public double stickerPrice() {
+        // Works as intended.
         double total = 0;
-        total+=this.getTrim().getCost();
-        Set<AvailablePackage> allPackages = this.getAvailablePackages();
-        Iterator<AvailablePackage> packageIterator = allPackages.iterator();
-        while(packageIterator.hasNext()){
-            total+=packageIterator.next().getCost();
+        total += this.getTrim().getCost();
+        System.out.println("TRIMID: " + this.getTrim().getTrimId());
+        for (AvailablePackage P : this.getAvailablePackages()) {
+            System.out.println("AP ID: " + P.getAvailableId() + P.getPackage().getPackageName());
+            total += P.getCost();
         }
         return total;
     }
+
     @Override
     public String toString() {
         return "VIN: " + vin + " (ID " + automobileId + ")";
@@ -90,6 +97,5 @@ public class Automobile {
     public void setTrim(Trim trim) {
         this.trim = trim;
     }
-
 
 }
