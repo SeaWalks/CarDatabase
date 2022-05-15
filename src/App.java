9b58fb16d756e3dac88c073e12c79d87d1efacd6
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2022-05-14 17:33:22 -0700

    added junction tables, need fix availablePackage

--- src/App.java
+++ src/App.java
@@ -9,4 +9,17 @@ public class App {
         EntityManager em = factory.createEntityManager();
         System.out.println("Hopefully this works.");
     }
+    public double stickerPrice(){
+        /*Returns a Set of all Feature objects that this Automobile has 
+        because of its Model, Trim, or chosen Packages. The HashSet class
+        and its addAll method will be helpful here.*/
+        return 0;
+    }
+    public Set<Feature> getFeatures(){
+        /*Returns the "sticker price" of the automobile: the sum of the Trim
+         cost, plus the costs of all packages added to the automobile. Do not
+        try to write a JPQL query to do this; you have all the data you need 
+        among the fields of the Automobile object and the methods of its related 
+        classes. */
+    }
 }
